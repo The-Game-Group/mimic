b2b870e9a0afe55299596ab84c5440d00792d44e
Ohin Taylor <kazani@kazani.dev>
2024-03-31 14:11:05 -0500

    Make it build on Nix

---
+++ default.nix
@@ -0,0 +1,58 @@
+with import <nixpkgs> { };
+
+let
+  graphics_dependencies = [
+    libGL
+    xorg.libX11
+    xorg.libfontenc
+    xorg.libICE
+    xorg.libSM
+    xorg.libXau
+    xorg.libXaw
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXdmcp
+    xorg.libXext
+    xorg.libXi
+    xorg.libXinerama
+    xorg.libxkbfile
+    xorg.libXpm
+    xorg.libXrandr
+    xorg.libXres
+    xorg.libXScrnSaver
+    xorg.libXtst
+    xorg.libXv
+    xorg.libXxf86vm
+    xorg.xcbutilwm
+    xorg.xcbutilimage
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+    xorg.xcbutil
+    xcb-util-cursor
+  ];
+in stdenv.mkDerivation {
+  name = "mimic";
+  buildInputs = [
+    clang-tools
+    cmake
+    git
+    ninja
+    pre-commit
+    pkg-config
+
+    libuuid
+    expat
+    bison
+    flex
+    zlib
+    libxml2
+    libiconv
+    libffi
+    python311Packages.jinja2
+  ] ++ graphics_dependencies;
+
+  shellHook = ''
+    pre-commit install
+  '';
+}
